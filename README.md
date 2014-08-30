419744d1c5710b53b82540fa113bc302f1742b78
r4dian <r4dian@users.noreply.github.com>
2014-08-30 20:38:59 +0100

    Initial Upload
    Source & Windows .exe file.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 3ds-to-anaglyphic
 =================
 
-Batch convert 3DS photos to red-cyan anaglyphic images with Python &amp; PIL
+Batch convert 3DS photos to red-cyan anaglyphic images with Python 3 &amp; ~~PIL~~ Pillow. 
+
+Converts 3DS photos to anaglyphic images for viewing on devices without 3d screens using your (my) 2 coloured 80s nerd glasses.
+
+Requirements: A version of Pillow from after 25 Jul 2014 which is when MPO format support was added.
+
+Windows .exe file included for people who can't/won't install Python.
+
+Currently only produces images for red (left-eye) / cyan (right eye) glasses.
---
+++ source/3ds2anaglyph.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys, os
+from PIL import Image
+from PIL import MpoImagePlugin
+
+from PIL.ImageOps import grayscale as greyscale
+from PIL.ImageOps import colorize as colourise
+
+def conv(files):
+    for file in files:
+        name, ext = os.path.splitext(file)
+        im = Image.open(file)
+        im.seek(0)
+        l = im.copy()
+        im.seek(1)
+        r = im.copy()
+
+        l = greyscale(l)
+        l = colourise(l, (0,0,0),(255,0,0))
+
+        r = greyscale(r)
+        r = colourise(r, (0,0,0),(0,255,255))
+
+        o = Image.blend(l,r,0.5)
+        o.save(name + ".jpg", "JPEG")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+       print("Usage: convert.py file1 file2 ...")
+       input("Press Enter to continue...")
+       exit()
+    conv(sys.argv[1:])
+    exit()
